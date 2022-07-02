1843943760889495c6a68c9306296474722faf88
Connor Tyrrell <connor@brewgene.com>
2022-07-02 13:41:20 -0400

    Formatting cleanup of Game Board

--- src/containers/GameBoard/GameBoard.js
+++ src/containers/GameBoard/GameBoard.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import { Button, Text, Icon, IconButton, Avatar } from '../../components';
+import { Button, Text, Icon, Divider, Avatar } from '../../components';
 import styles from './GameBoardStyles';
 import { colors, typography } from '../../styles';
 import { DataStore } from '../../utils';
@@ -175,58 +175,65 @@ const GameBoard = (props) => {
       <KeyboardAwareScrollView style={styles.scrollablePageWrapper} keyboardShouldPersistTaps='always'>
         {players.map((player, index) => {
           return (
-            <View key={index} style={styles.playerWrapper}>
-              <View style={{flexDirection: 'row'}}>
-                <View style={{alignItems: 'center', justifyContent: 'center', paddingRight: 10}}>
-                  <Avatar name={player.name} size={(typography.fontSizeXL + typography.fontSizeS) * 1.5} textSize='XL' />
+            <>
+              {index !== 0 && (
+                <View style={{paddingTop: 3, paddingBottom: 3}}>
+                  <Divider />
                 </View>
-                <View style={{flexDirection: 'column'}}>
-                  <View style={styles.playerHeader}>
-                    {winningPlayerId === player.id && (
-                      <Icon name='winner' color={colors.primaryBlue} size={typography.fontSizeXL} />
-                    )}
-                    {whichPlayersTurn === player.id && gameStatus === 'inProgress' && (
-                      <Icon name='play' color={colors.primaryBlue} size={typography.fontSizeXL} />
-                    )}
-                    <Text size='XL' bold>
-                      {player.name}:{' '}
-                    </Text>
-                    <Text size='XL'>
-                      {scores.find(score => score.playerId === player.id).score}
-                    </Text>
+              )}
+              <View key={index} style={styles.playerWrapper}>
+                <View style={{ flexDirection: 'row' }}>
+                  <View style={{ alignItems: 'center', justifyContent: 'center', paddingRight: 10 }}>
+                    <Avatar name={player.name} size={(typography.fontSizeXL + typography.fontSizeS) * 1.5} textSize='XL' />
                   </View>
-                  <View style={styles.turnsWrapper}>
-                    {turns.filter(turn => turn.playerId === player.id).length > 0 ? (
-                      turns.filter(turn => turn.playerId === player.id).map((turn, index) => {
-                        return (
-                          <View key={index} style={{flexDirection: 'row'}}>
-                            <Text size='S'>
-                              {index > 0 ? ', ' : ''}
-                            </Text>
-                            {turn.skipped ? (
-                              <Icon name='skip' size={typography.fontSizeS} color={colors.primaryBlue} />
-                            ) : (
-                              <>
-                                <Text size='S'>
-                                  {turn.score}
-                                </Text>
-                                {turn.wentOver ? (
-                                  <Icon name='wentOver' size={typography.fontSizeS} color={colors.red} />
-                                ) : null}
-                              </>
-                            )}
-                          </View>
-                        )
-                      })
-                    ) : (
-                      <Text size='S'>
-                        Hasn't had a turn yet
+                  <View style={{ flexDirection: 'column' }}>
+                    <View style={styles.playerHeader}>
+                      {winningPlayerId === player.id && (
+                        <Icon name='winner' color={colors.primaryBlue} size={typography.fontSizeXL} />
+                      )}
+                      {whichPlayersTurn === player.id && gameStatus === 'inProgress' && (
+                        <Icon name='play' color={colors.primaryBlue} size={typography.fontSizeXL} />
+                      )}
+                      <Text size='XL' bold>
+                        {player.name}:{' '}
                       </Text>
-                    )}
+                      <Text size='XL'>
+                        {scores.find(score => score.playerId === player.id).score}
+                      </Text>
+                    </View>
+                    <View style={styles.turnsWrapper}>
+                      {turns.filter(turn => turn.playerId === player.id).length > 0 ? (
+                        turns.filter(turn => turn.playerId === player.id).map((turn, index) => {
+                          return (
+                            <View key={index} style={{ flexDirection: 'row' }}>
+                              <Text size='S'>
+                                {index > 0 ? ', ' : ''}
+                              </Text>
+                              {turn.skipped ? (
+                                <Icon name='skip' size={typography.fontSizeS} color={colors.primaryBlue} />
+                              ) : (
+                                <>
+                                  <Text size='S'>
+                                    {turn.score}
+                                  </Text>
+                                  {turn.wentOver ? (
+                                    <Icon name='wentOver' size={typography.fontSizeS} color={colors.red} />
+                                  ) : null}
+                                </>
+                              )}
+                            </View>
+                          )
+                        })
+                      ) : (
+                        <Text size='S'>
+                          Hasn't had a turn yet
+                        </Text>
+                      )}
+                    </View>
                   </View>
                 </View>
               </View>
-            </View>
+            </>
           )
         })}
         {gameStatus === 'inProgress' ? (
@@ -287,12 +294,12 @@ const GameBoard = (props) => {
           </>
         ) : (
           <>
-          <View style={{padding: 10, alignItems: 'center'}}>
-            <Text bold size='XL'>
-              {players.find((p) => p.id === winningPlayerId).name} won!
-            </Text>
-          </View>
-          <View style={styles.buttonsWrapper}>
+            <View style={{ padding: 10, alignItems: 'center' }}>
+              <Text bold size='XL'>
+                {players.find((p) => p.id === winningPlayerId).name} won!
+              </Text>
+            </View>
+            <View style={styles.buttonsWrapper}>
               <View style={styles.twoButtonWrapper}>
                 <Button onPress={() => undoTurn()} text='Undo Last Turn' disabled={turns.length === 0} />
               </View>
@@ -300,7 +307,7 @@ const GameBoard = (props) => {
                 <Button onPress={() => playAgain()} text='Play Again' />
               </View>
             </View>
-            </>
+          </>
         )}
       </KeyboardAwareScrollView>
     </View>
